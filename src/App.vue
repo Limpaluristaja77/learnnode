4ddf74e8a4630e20effc70b1ad59d97c0217e7ae
Andri Reinek <andri.reinek@tptlive.ee>
2025-01-20 21:57:36 +0200

    Add content box

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,40 @@
 import { ref } from "vue";
 
 let message = ref("Gooner");
+let items = ref(['Sai','Piim','Viin','Õlu']);
+
+
+function addItem(){
+    if(message.value.trim() !==''){
+    items.value.push(message.value.trim());
+    }
+    message.value = '';
+
+}
+
 
 </script>
 
 <template>
-    <div class="container">
-        <h1>{{ message.split('').reverse().join('') }}</h1>
-        <button class="button is-primary" @click="message='Click Clack'">Click me</button>
-        <input class="input" v-model="message">
+    <div class="container mt-2">
+        <div class="field has-addons">
+            <div class="control">
+                <input class="input" type="text" v-model="message" @keypress.enter="addItem">
+            </div>
+            <div class="control">
+                <button class="button is-info" @click="addItem">
+                    Add item
+                </button>
+            </div>
+        </div>
+        <div class="content">
+            <h3>All Items</h3>
+            <ul>
+                <li v-for="item in items">{{ item }}</li>
+            </ul>
+        </div>
     </div>
+
 </template>
 
 <style></style>

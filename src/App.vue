e1da914d81646ef36b465a94fee3c1d9b895bace
Andri Reinek <andri.reinek@tptlive.ee>
2025-01-21 11:39:17 +0200

    add modal

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,31 @@
 <script setup>
+import { ref } from 'vue';
 
-import { computed, ref } from "vue";
-import ItemList from "./ItemList.vue";
+let modalActive = ref(false);
 
-let message = ref("");
-let i = 1;
-let items = ref([
-    { id: i++, name: 'Sai', isDone: false },
-    { id: i++, name: 'Piim', isDone: true },
-    { id: i++, name: 'Viin', isDone: false },
-    { id: i++, name: 'Õlu', isDone: true },
-]);
-
-
-function addItem() {
-    if (message.value.trim() !== '') {
-        items.value.push({ id: i++, name: message.value.trim(), isDone: false });
+document.body.addEventListener('keydown', event => {
+    if(event.key === 'Escape'){
+        modalActive.value = false;
     }
-    message.value = '';
-
-}
-
-let doneItems = computed(() => items.value.filter(item => item.isDone));
-let ToDoItems = computed(() => items.value.filter(item => !item.isDone));
-
+});
 
 </script>
 
 <template>
-    <div class="container mt-2">
-        <div class="field has-addons">
-            <div class="control">
-                <input class="input" type="text" v-model="message" @keypress.enter="addItem">
-            </div>
-            <div class="control">
-                <button class="button is-info" @click="addItem">
-                    Add item
-                </button>
-            </div>
+    <div class="container">
+        <section>
+           <button class="button is-primary" @click="modalActive=true">Modal Active</button>
+        </section>
+
+    </div>
+    <div class="modal" :class="{'is-active' : modalActive}">
+        <div class="modal-background" @click="modalActive=false"></div>
+        <div class="modal-content">
+            <p class="image is-4by3">
+                <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
+            </p>
         </div>
-        <div class="content">
-            <ItemList :items="items" title="Items"></ItemList> 
-            <ItemList :items="doneItems" title="Done Items"></ItemList>   
-            <ItemList :items="ToDoItems" title="ToDo Items"></ItemList>           
-        </div>
+        <button class="modal-close is-large" aria-label="close"@click="modalActive=false" ></button>
     </div>
 
 </template>

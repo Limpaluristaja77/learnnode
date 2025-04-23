c28ce2156963e6e0448313817294172d8cb9abcf
Andri Reinek <andri.reinek@tptlive.ee>
2025-04-23 13:50:59 +0300

    Leaflet home

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -5,9 +5,9 @@ import 'leaflet/dist/images/marker-shadow.png'
 import 'leaflet/dist/images/marker-icon.png'
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import {useId, onMounted, watch } from 'vue';
+import { useId, onMounted, watch } from 'vue';
 
-const {center, zoom} = defineProps(['center', 'zoom']);
+const { center, zoom } = defineProps(['center', 'zoom']);
 const id = 'map-' + useId();
 let map;
 onMounted(() => {
@@ -16,7 +16,10 @@ onMounted(() => {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
-    var marker = L.marker([59.4270041078148, 24.743395645775344]).addTo(map);
+    var marker = L.marker([59.33534147342539, 25.15247708130768]).addTo(map);
+    var latlngs = [[59.335332268985255, 25.152326705288193], [59.33541116811839, 25.152580286588083], [59.335338776137185, 25.152656839433337], [59.33526231702326, 25.152411232388157]];
+
+    var polygon = L.polygon(latlngs, { color: 'red' }).addTo(map);
 });
 
 watch(() => center, (center, oldCenter) => {
@@ -36,3 +39,4 @@ div {
     height: 90vh;
 }
 </style>
+
--- src/pages/Leaflet.vue
+++ src/pages/Leaflet.vue
@@ -5,7 +5,7 @@ let center = ref([59.4270041078148, 24.743395645775344]);
 let zoom = ref(19);
 </script>
 <template>
-    <button class="button is-primary" @click="center=[59.281174189596285, 25.214959387921756]">Go sleep</button>
+    <button class="button is-primary" @click="center=[59.33534147342539, 25.15247708130768]">Go Home</button>
     <input type="range" min="1" max="18" v-model="zoom">
     <LeafletMap :center="center" :zoom="zoom"> </LeafletMap>
     <LeafletMap :center="[24.4270041078148, 59.743395645775344]" :zoom="18"> </LeafletMap>

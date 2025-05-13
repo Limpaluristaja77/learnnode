2bc654d0de73d7fe83d704bb6c91d00197d7bd91
Andri Reinek <andri.reinek@tptlive.ee>
2025-05-13 11:24:38 +0300

    add terminal clock

--- bin/terminal.js
+++ bin/terminal.js
@@ -4,49 +4,9 @@ function write(message){
 
 const ESC = '\x1b';
 
-write(ESC + "[31m");
-write(ESC + "[42m");
-write("message");
-write(ESC + "[0m");
-write("\n");
-
-write(ESC + "[31m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[32m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[33m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[34m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[35m"); write("message"); write(ESC + "[0m"); write("\n");
-
-write(ESC + "[41m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[42m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[43m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[44m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[45m"); write("message"); write(ESC + "[0m"); write("\n");
-
-write(ESC + "[1m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[2m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[3m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[4m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[5m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[7m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[8m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[9m"); write("message"); write(ESC + "[0m"); write("\n");
-
-
-
-write(ESC + "[31m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[1;31m"); write("message"); write(ESC + "[0m"); write("\n");
-write(ESC + "[2;31m"); write("message"); write(ESC + "[0m"); write("\n");
-
-
-for(let i = 0; i<256; i ++){
-    write(ESC + `[48;5;${i}m`); write(" "); 
-}
-
-write(ESC + "[0m");
-
-
-for(let i = 0; i<256; i ++){
-    write(ESC + `[48;2;${i};0;0m`); write(" "); 
-}
-
-write(ESC + "[0m");
+let time = new Date().toTimeString().substring(0,8);
+write(time); 
+setInterval(() => {
+    time = new Date().toTimeString().substring(0,8);
+    write(ESC + "[8D"); write(time)
+}, 1000);

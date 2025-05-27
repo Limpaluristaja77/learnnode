eec779bfca9860e989ae421fc6b36bfe5a66a545
Andri Reinek <andri.reinek@tptlive.ee>
2025-05-27 11:04:27 +0300

    scraper

--- bin/WUMOscraper.js
+++ bin/WUMOscraper.js
@@ -1,44 +1,43 @@
 import axios from "axios";
-import * as cheerio from 'cheerio';
-import fs from 'fs';
-
+import * as cheerio from "cheerio";
+import fs from "fs";
 
 function cacheGet(name) {
-    if (fs.existsSync(`./cache/${name}.html`)) {
-        return fs.readFileSync(`./cache/${name}.html`);
-    }
-    return false;
-
+  if (fs.existsSync(`./cache${name}.html`)) {
+    return fs.readFileSync(`./cache${name}.html`);
+  }
+  return false;
 }
 
 function cacheSet(name, value) {
-    if (!fs.existsSync('./cache')) {
-        fs.mkdirSync('./cache');
-    }
-    fs.writeFileSync(`./cache/${name}.html`, value);
+  if (!fs.existsSync("./cache")) {
+    fs.mkdirSync("./cache");
+  }
+  fs.writeFileSync(`./cache${name}.html`, value);
 }
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
+let baseUrl = "https://wumo.com";
+let url = baseUrl + "";
+for (let i = 0; i < 10; i++) {
+  let name = url.replaceAll("/", " ").replaceAll(":", "");
+  let data = cacheGet(name);
+  if (!data) {
+    console.log("LIVE REQUEST!!!!!!!");
+    await sleep(1000);
+    let res = await axios.get(url);
+    data = res.data;
+    cacheSet(name, data);
+  }
 
-for (let i = 3091; i > 3081; i--) {
-    let data = cacheGet(i);
-    if (!data) {
-        console.log('LIVE REQUEST')
-        await sleep(1000);
-        let res = await axios.get(`https://xkcd.com/${i}/`);
-        data = res.data;
-        cacheSet(i, data);
-    }
-    const $ = cheerio.load(data);
-
-    let src = $('#comic img').attr('src');
-    let text = $('#comic img').attr('title');
-    let title = $('#comic img').attr('alt');
-
-    console.log(src);
-    console.log(text);
-    console.log(title);
-    console.log('---------------------');
+  //console.log(res.data);
+  const $ = cheerio.load(data);
+  let src = baseUrl + $("article.wumo img").attr("src");
+  let title = $("article.wumo img").attr("alt");
+  console.log(src);
+  console.log(title);
+  url = baseUrl + $("article.wumo a.prev").attr("href");
+  console.log(url);
 }

7ed9ac48cbcabc67f535876b7a32752737cb74ed
Andri Reinek <andri.reinek@tptlive.ee>
2025-06-02 15:09:04 +0300

    scraper finish

--- bin/scraper.js
+++ bin/scraper.js
@@ -2,13 +2,11 @@ import axios from "axios";
 import * as cheerio from 'cheerio';
 import fs from 'fs';
 
-
 function cacheGet(name) {
     if (fs.existsSync(`./cache/${name}.html`)) {
         return fs.readFileSync(`./cache/${name}.html`);
     }
     return false;
-
 }
 
 function cacheSet(name, value) {
@@ -22,23 +20,30 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-for (let i = 3091; i > 3081; i--) {
-    let data = cacheGet(i);
+let baseUrl = 'https://explosm.net';
+let url = baseUrl + '/comics/latest';
+for (let i = 0; i < 25; i++) {
+    let name = url.replaceAll('/', '').replaceAll(':', '');
+    let data = cacheGet(name);
     if (!data) {
-        console.log('LIVE REQUEST')
+        console.log('LIVE REQUEST!!!!!!');
         await sleep(1000);
-        let res = await axios.get(`https://www.qwantz.com/index.php?comic=${i}`);
+        let res = await axios.get(url);
         data = res.data;
-        cacheSet(i, data);
+        cacheSet(name, data);
     }
+
+    
     const $ = cheerio.load(data);
+    let img = $('div[class*="MainComic__ComicImage"] img');
+    let src = img.attr('src');
+    let title = img.attr('alt') || 'Cyanide & Happiness';
 
-    let src = $('#').attr('src');
-    let text = $('#').attr('title');
-    let title = $('#').attr('alt');
-
+    
+    if (src && !src.startsWith('http')) src = baseUrl + src;
     console.log(src);
-    console.log(text);
     console.log(title);
-    console.log('---------------------');
+    let prev = $('div[class*="ComicSelector__Container"] a').first().attr('href');
+    if (!prev) break;
+    url = prev.startsWith('/') ? baseUrl + prev : prev;
 }
